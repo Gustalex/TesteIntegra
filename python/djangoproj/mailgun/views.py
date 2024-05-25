b9b0111b452cfc3ca2b6ab860b2f2d4f3f6515a0
Gustalex <alexandregustavo00@gmail.com>
2024-05-25 11:50:13 -0300

    fix: bad request adicionando @csrf_exempt e finaliza a integração

--- python/djangoproj/mailgun/views.py
+++ python/djangoproj/mailgun/views.py
@@ -1,12 +1,15 @@
 from django.core.mail import BadHeaderError, send_mail as django_send_mail
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from decouple import config
 
+@csrf_exempt
 def send_mail(request):
     subject = request.POST.get("subject", "")
     message = request.POST.get("message", "")
     to_email = request.POST.get("to_email", "")
     from_email = config("EMAIL_HOST_USER")
+
     if subject and message and from_email and to_email:
         try:
             django_send_mail(subject, message, from_email, [to_email])
